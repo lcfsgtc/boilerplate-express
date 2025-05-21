e422ecb993a7676f50c1a4dca52a748ca4245244
lcfsgtc <lcfsgtc@126.com>
2025-05-21 09:50:18 +0800

    6666

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 //app.use('/public', express.static(__dirname + "/public"));
 
 app.use(express.static(__dirname + "/public"));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + "/public"));
 if (!process.env.DISABLE_XORIGIN) {
   app.use((req, res, next) => {
     const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
